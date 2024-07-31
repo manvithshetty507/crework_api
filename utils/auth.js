454a242be5af709d8b844e6b3b08c44bac61b23a
manvithshetty507 <manvithshetty18@gmail.com>
2024-07-31 17:39:33 +0530

    incraesed token exp time

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
     const key = process.env.NEXT_PUBLIC_JWT_SECRET;
-    return jwt.sign({ id: userId }, key, { expiresIn: '1h' });
+    return jwt.sign({ id: userId }, key, { expiresIn: '24h' });
 };
 
 module.exports = generateToken;
